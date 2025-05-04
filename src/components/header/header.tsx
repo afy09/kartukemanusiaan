ff620960b58811c1a82a951d806e97bf9e300c4e
budi@gmail.com <mhmd.raffli09@gmail.com>
2025-05-05 03:20:00 +0700

    update wa

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Menu from "../../pages/menu";
+// import Menu from "../../pages/menu";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const slides = ["/images/KK_depan.png", "/images/KK_belakang.png", "/images/KK_depan.png", "/images/KK_belakang.png"]; // Array gambar slider
@@ -22,7 +23,7 @@ const Header = () => {
 
   return (
     <>
-      <Menu />
+      {/* <Menu /> */}
       <div className=" w-full max-w-4xl mx-auto overflow-hidden rounded-lg shadow-lg mt-4">
         {/* Slides */}
         <div className="flex transition-transform duration-700 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
@@ -30,6 +31,25 @@ const Header = () => {
             <img key={index} src={slide} alt={`Slide ${index + 1}`} className="w-full object-cover" />
           ))}
         </div>
+      </div>
+      <hr />
+      <div className="flex gap-2 items-center mt-4 justify-center bg-black px-4 py-2 text-white rounded-lg ">
+        {/* <div className="px-2  border bg-orange-600 text-white rounded-full">2</div> */}
+        <h1 className="text-center font-medium text-lg md:text-xl ">Daftar</h1>
+      </div>
+
+      <div className="flex gap-2 w-full text-center mt-5 mb-8 text-xs md:text-base ">
+        <Link to="/tentang" className="w-full">
+          <div className="border border-orange-600 px-7 py-2 w-full rounded-lg text-orange-600 hover:bg-orange-600 hover:text-white">Tentang</div>
+        </Link>
+
+        <Link to="/visimisi" className="w-full ">
+          <div className="border border-orange-600 px-7 py-2 w-full rounded-lg text-orange-600 hover:bg-orange-600 hover:text-white  ">Visi Misi</div>
+        </Link>
+
+        <Link to="/program" className="w-full">
+          <div className="border border-orange-600 px-7 py-2 w-full rounded-lg text-orange-600 hover:bg-orange-600 hover:text-white">Program</div>
+        </Link>
       </div>
       {/* Indicator */}
       {/* <div className=" mt-2 -translate-x-1/2 flex justify-center space-x-2">
--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,89 +1,45 @@
-import React, { useState, useEffect } from "react";
-// import Berita from "../berita";
+import React, { useState } from "react";
 import Foother from "../../components/foother/foother";
-import axios from "axios";
-import Lottie from "lottie-react";
-import AnimatinSukses from "./icon/animasi-succses-udin.json";
 
 const Form = () => {
   const [nama, setnama] = useState<string>("");
-  const [jenis_kelamin, setjenis_kelamin] = useState<string>("");
-  const [tempat, settempat] = useState<string>("");
-  const [tanggal_lahir, settanggal_lahir] = useState<string>("");
-  const [provinsi, setprovinsi] = useState<string>("");
-  const [kabupaten, setkabupaten] = useState<string>("");
-  const [kecamatan, setkecamatan] = useState<string>("");
-  const [kelurahan, setkelurahan] = useState<string>("");
   const [email, setemail] = useState<string>("");
   const [no_hp, setno_hp] = useState<string>("");
-
-  const [ktp, setktp] = useState<File | null>(null);
+  const [alamat, setAlamat] = useState<string>("");
+  const [jenisKartu, setJenisKartu] = useState<string>("");
 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const [count, setCount] = useState(null);
-
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    setIsSubmitting(true);
-    const formData = new FormData();
-    formData.append("nama", nama);
-    formData.append("jenis_kelamin", jenis_kelamin);
-    formData.append("tempat", tempat);
-    formData.append("tanggal_lahir", tanggal_lahir);
-    formData.append("provinsi", provinsi);
-    formData.append("kabupaten", kabupaten);
-    formData.append("kecamatan", kecamatan);
-    formData.append("kelurahan", kelurahan);
-    formData.append("email", email);
-    formData.append("no_hp", no_hp);
-
-    formData.append("ktp", ktp as Blob);
-
-    try {
-      const response = await axios.post("https://relawan.rekapitung.id/api/formulir", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-
-      if (response.status === 201) {
-        setShowPopup(true);
-      }
-    } catch (error) {
-      alert("gagal mengirim data");
-      console.error("Error:", error);
-    } finally {
-      setIsSubmitting(false);
-    }
+    setShowPopup(true); // hanya munculkan popup, belum redirect
   };
 
-  useEffect(() => {
-    // Fungsi untuk mengambil data dari backend
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://relawan.rekapitung.id/api/formulir/count");
-        if (!response.ok) {
-          throw new Error("Gagal mengambil data");
-        }
-        const data = await response.json();
-        setCount(data.count);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  const handleSendToWhatsApp = () => {
+    const message =
+      `*📄 Pendaftaran Kartu Kemanusiaan*\n\n` +
+      `*Nama:* ${nama}\n` +
+      `*Email:* ${email}\n` +
+      `*Nomor WhatsApp:* ${no_hp}\n` +
+      `*Alamat Pengiriman:* ${alamat}\n` +
+      `*Jenis Kartu:* ${jenisKartu}\n` +
+      `*Harga:* ${jenisKartu === "Reguler" ? "Rp. 20.000" : "Rp. 10.000"} (Belum Termasuk Ongkos Kirim)\n\n` +
+      `*Silakan balas pesan ini dengan mengirimkan foto Profile Anda untuk verifikasi.*\n\n` +
+      `Terima kasih atas partisipasi Anda! ❤️`;
 
-    fetchData();
-  }, []);
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappLink = `https://wa.me/6281210681178?text=${encodedMessage}`;
+
+    window.open(whatsappLink, "_blank");
+    setShowPopup(false);
+  };
 
   return (
     <>
       <div className="flex gap-2 items-center justify-center my-4">
-        <h1 className="text-black text-center font-bold text-[22px] md:text-[25px]">
-          Formulir Pembuatan <br /> Kartu Kemanusiaan
-        </h1>
+        <h1 className="text-black text-center font-bold text-[22px] md:text-[25px]">Formulir Kartu Kemanusiaan</h1>
       </div>
       <div className="px-4 py-4 border border-gray-400 rounded-lg mt-4 mb-4">
         <div className={`${isExpanded ? "" : "max-h-[700px] overflow-hidden"}`}>
@@ -92,39 +48,6 @@ const Form = () => {
             <div>
               <label className="block mb-1 font-semibold">Nama Lengkap</label>
               <input type="text" name="nama" placeholder="Masukkan Nama Lengkap" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={nama} onChange={(e) => setnama(e.target.value)} />
-            </div>
-
-            {/* Jenis Kelamin */}
-            <div>
-              <label className="block mb-1 font-semibold">Jenis Kelamin</label>
-              <select name="jenis_kelamin" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={jenis_kelamin} onChange={(e) => setjenis_kelamin(e.target.value)}>
-                <option value="" disabled>
-                  Pilih Jenis Kelamin
-                </option>
-                <option value="laki-laki">Laki-Laki</option>
-                <option value="wanita">Wanita</option>
-              </select>
-            </div>
-
-            {/* Tempat */}
-            <div>
-              <label className="block mb-1 font-semibold">Tempat Lahir</label>
-              <input type="text" name="tempat" placeholder="Masukkan Tempat Lahir" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={tempat} onChange={(e) => settempat(e.target.value)} />
-            </div>
-
-            {/* Tanggal Lahir */}
-            <div>
-              <label className="block mb-1 font-semibold">Tanggal Lahir</label>
-              <input type="date" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={tanggal_lahir} onChange={(e) => settanggal_lahir(e.target.value)} />
-            </div>
-
-            {/* Alamat */}
-            <div>
-              <label className="block mb-1 font-semibold">Alamat</label>
-              <input type="text" name="provinsi" placeholder="Provinsi" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={provinsi} onChange={(e) => setprovinsi(e.target.value)} />
-              <input type="text" name="kabupaten" placeholder="Kabupaten / Kota" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none mt-5" value={kabupaten} onChange={(e) => setkabupaten(e.target.value)} />
-              <input type="text" name="kecamatan" placeholder="Kecamatan" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none mt-5" value={kecamatan} onChange={(e) => setkecamatan(e.target.value)} />
-              <input type="text" name="kelurahan" placeholder="Kelurahan" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none mt-5" value={kelurahan} onChange={(e) => setkelurahan(e.target.value)} />
             </div>
 
             {/* Email */}
@@ -139,51 +62,77 @@ const Form = () => {
               <input type="text" name="no_hp" placeholder="Masukkan Nomor WhatsApp" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={no_hp} onChange={(e) => setno_hp(e.target.value)} />
             </div>
 
-            {/* Foto Profile */}
+            {/* Alamat */}
             <div>
-              <label className="block mb-1 font-semibold">Foto Profile</label>
-              <input
-                type="file"
-                name="ktp"
-                className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none"
-                onChange={(e) => {
-                  const file = e.target.files?.[0] || null; // Mendapatkan file pertama yang dipilih atau null
-                  setktp(file); // Memperbarui state dengan file
-                }}
-              />
+              <label className="block mb-1 font-semibold">Alamat Pengiriman</label>
+              <textarea rows={4} placeholder="Masukkan Alamat" className="bg-gray-100 px-3 py-2 w-full rounded-lg outline-none" value={alamat} onChange={(e) => setAlamat(e.target.value)} />
             </div>
 
-            {/* Submit Button */}
-            <div className="flex justify-between gap-3 mb-10">
-              <div className=" text-black font-medium">
-                Jumlah Pendaftar : <span className="text-orange-600">{count !== null ? count : "Memuat..."}</span>
+            <div>
+              <label className="block mb-1 font-semibold">Jenis Kartu</label>
+              <div className="flex items-center gap-4">
+                <label className="flex items-center gap-2">
+                  <input className="accent-orange-600" type="radio" name="jenis_kartu" value="Reguler" checked={jenisKartu === "Reguler"} onChange={(e) => setJenisKartu(e.target.value)} />
+                  Reguler
+                </label>
+                <label className="flex items-center gap-2">
+                  <input className="accent-orange-600" type="radio" name="jenis_kartu" value="E-Money" checked={jenisKartu === "E-Money"} onChange={(e) => setJenisKartu(e.target.value)} />
+                  E-Money
+                </label>
               </div>
+            </div>
+
+            {jenisKartu === "Reguler" && <p className="text-sm text-gray-700 mb-1">Harga: Rp. 20.000 (Belum Termasuk Ongkos Kirim)</p>}
+            {jenisKartu === "E-Money" && <p className="text-sm text-gray-700 mb-1">Harga: Rp. 100.000 (Belum Termasuk Ongkos Kirim)</p>}
+
+            {/* Submit Button */}
+            <div className="flex justify-end gap-3 mb-10">
               <button type="submit" className={`bg-orange-600 px-7 py-2 text-white rounded-lg`}>
                 {isSubmitting ? "Mengirim..." : "Submit"}
               </button>
             </div>
           </form>
         </div>
-        <div className="flex justify-start mt-4 mb-4">
-          <button type="button" className="text-orange-600  outline-none" onClick={() => setIsExpanded(!isExpanded)}>
-            {isExpanded ? "Sembunyikan" : "Selanjutnya..."}
-          </button>
-        </div>
-
-        {/* <Berita /> */}
         <Foother />
       </div>
 
       {showPopup && (
-        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-999999">
-          <div className="bg-white px-12 py-8 rounded-2xl text-center flex flex-col justify-center">
-            <div className=" flex justify-center">
-              <Lottie className="w-52 h-52" animationData={AnimatinSukses} loop={false} />
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+          <div className="bg-white px-8 py-6 rounded-2xl w-[90%] max-w-md text-center">
+            <div className="flex justify-center mb-4">
+              <img className="w-16 h-20" src="/images/bk.png" alt="logo" />
             </div>
-            <p className="text-black font-semibold text-xl">Berhasil Mendaftar</p>
-            <div className="w-full">
-              <button className="mt-4 px-4 py-2 border border-orange-600 text-orange-600 w-full rounded-lg " onClick={() => window.location.reload()}>
-                Lanjut
+            <p className="text-black font-semibold text-xl mb-4">Konfirmasi Data Anda</p>
+
+            <div className="text-left text-sm text-black mb-4 space-y-1">
+              <div className="flex justify-between">
+                <strong className="w-16">Nama :</strong>
+                <span>{nama}</span>
+              </div>
+              <div className="flex justify-between">
+                <strong className="w-16">Email :</strong>
+                <span>{email}</span>
+              </div>
+              <div className="flex justify-between">
+                <strong className="w-28">No WhatsApp :</strong>
+                <span> {no_hp}</span>
+              </div>
+              <div className="flex justify-between">
+                <strong className="w-28">Jenis Kartu :</strong>
+                <span> {jenisKartu}</span>
+              </div>
+              <div className="flex flex-col ">
+                <strong className="w-16">Alamat :</strong>
+                <span>{alamat}</span>
+              </div>
+            </div>
+
+            <div className="flex gap-2 mt-4">
+              <button className="flex-1 bg-orange-600 text-white py-2 rounded-lg" onClick={handleSendToWhatsApp}>
+                Kirim
+              </button>
+              <button className="flex-1 border border-orange-600 text-orange-600 py-2 rounded-lg" onClick={() => setShowPopup(false)}>
+                Perbaiki
               </button>
             </div>
           </div>
