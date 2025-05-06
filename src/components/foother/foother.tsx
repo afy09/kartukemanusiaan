41ea8dcc5b3691d1f581ddd14135616e0e4bada2
budi@gmail.com <mhmd.raffli09@gmail.com>
2025-05-06 09:20:59 +0700

    update

--- src/components/foother/foother.tsx
+++ src/components/foother/foother.tsx
@@ -14,7 +14,7 @@ const Foother = () => {
       </div>
 
       <div className="text-black text-xs md:text-sm text-center mt-3">Yayasan Belas Kasih</div>
-      <div className="text-black text-xs md:text-sm text-center mt-1">JL. MAHKOTA PUTRS NO.1, SENTUL CITY - BOGOR 16810</div>
+      <div className="text-black text-xs md:text-sm text-center mt-1">JL. MAHKOTA PUTRA NO.1, SENTUL CITY - BOGOR 16810</div>
       <div className="text-black text-xs md:text-sm text-center mt-1">0821-2441-5600</div>
       {/* <div className="flex justify-center gap-3 mt-5 text-lg">
         <FaInstagram />
